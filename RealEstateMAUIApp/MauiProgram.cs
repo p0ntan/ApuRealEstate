e09bc81e35c8800448f54d4b715f7ff8069be156
Pontus Åkerberg <pontus.akerberg@hotmail.com>
2024-10-06 16:11:18 +0200

    Remove injection, make estateservice singleton instead

--- RealEstateMAUIApp/MauiProgram.cs
+++ RealEstateMAUIApp/MauiProgram.cs
@@ -15,8 +15,6 @@ namespace RealEstateMAUIApp
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-            builder.Services.AddSingleton<EstateService>();
-            builder.Services.AddTransient<MainPage>();
 
 #if DEBUG
             builder.Logging.AddDebug();
