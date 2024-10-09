a9760fe70c15cac24dd0357b95b4fe004ca8fb03
Pontus Åkerberg <pontus.akerberg@hotmail.com>
2024-10-09 17:53:49 +0200

    Add Automapper and Service class

---
+++ RealEstateBLL/BLLService.cs
@@ -0,0 +1,101 @@
+﻿// Created by Pontus Åkerberg 2024-10-05
+using RealEstateBLL.Estates;
+using RealEstateBLL.Manager;
+using RealEstateDTO;
+
+namespace RealEstateBLL;
+
+/// <summary>
+/// BLLServices is the entrypoint for the SL to the BLL.
+/// </summary>
+public class BLLService
+{
+    private EstateManager _estateManager;
+    public BLLService(EstateManager estateManager)
+    {
+        _estateManager = estateManager;
+    }
+    /// <summary>
+    /// Gets an estate from the given manager. 
+    /// </summary>
+    /// <param name="estateID">Id for the estate.</param>
+    /// <param name="estateManager">EstateManager to use and find estate.</param>
+    /// <returns>The found estate, null if none found.</returns>
+    public EstateDTO? GetEstate(int estateID)
+    {
+        Estate? estate = _estateManager.Get(estateID);
+
+        if (estate == null)
+            return null;
+
+        EstateMapper mapper = new();
+        EstateDTO? estateDTO = estate switch
+        {
+            Rowhouse rowhouse => mapper.MapDTO<Rowhouse, RowhouseDTO>(rowhouse),
+            Villa villa => mapper.MapDTO<Villa, VillaDTO>(villa),
+            Rental rental => mapper.MapDTO<Rental, RentalDTO>(rental),
+            Tenement tenement => mapper.MapDTO<Tenement, TenementDTO>(tenement),
+            Hospital hospital => mapper.MapDTO<Hospital, HospitalDTO>(hospital),
+            School school => mapper.MapDTO<School, SchoolDTO>(school),
+            University university => mapper.MapDTO<University, UniversityDTO>(university),
+            Shop shop => mapper.MapDTO<Shop, ShopDTO>(shop),
+            Factory factory => mapper.MapDTO<Factory, FactoryDTO>(factory),
+            Hotel hotel => mapper.MapDTO<Hotel, HotelDTO>(hotel),
+            Warehouse warehouse => mapper.MapDTO<Warehouse, WarehouseDTO>(warehouse),
+            _ => null
+        };
+
+        return estateDTO;
+    }
+
+    public (bool success, int newID) CreateEstate(EstateDTO estateDTO)
+    {
+        Estate? estate = ConvertDTOToEstate(estateDTO);
+
+        int newID = -1;
+
+        if (estate == null)
+            return (false, newID);
+
+        bool estateAdded = _estateManager.Add(estate);
+
+        if (estateAdded)
+            newID = estate.ID;
+
+        return (estateAdded,  newID);
+    }
+
+    public bool UpdateEstate(EstateDTO estateDTO)
+    {
+        Estate? estate = ConvertDTOToEstate(estateDTO);
+
+        if (estate == null)
+            return false;
+
+        bool estateUpdated = _estateManager.Change(estate.ID, estate);
+
+        return estateUpdated;
+    }
+
+    private static Estate? ConvertDTOToEstate(EstateDTO estateDTO)
+    {
+        EstateMapper mapper = new();
+        Estate? estate = estateDTO switch
+        {
+            RowhouseDTO rowhouse => mapper.MapDTO<RowhouseDTO, Rowhouse>(rowhouse),
+            VillaDTO villa => mapper.MapDTO<VillaDTO, Villa>(villa),
+            RentalDTO rental => mapper.MapDTO<RentalDTO, Rental>(rental),
+            TenementDTO tenement => mapper.MapDTO<TenementDTO, Tenement>(tenement),
+            HospitalDTO hospital => mapper.MapDTO<HospitalDTO, Hospital>(hospital),
+            SchoolDTO school => mapper.MapDTO<SchoolDTO, School>(school),
+            UniversityDTO university => mapper.MapDTO<UniversityDTO, University>(university),
+            ShopDTO shop => mapper.MapDTO<ShopDTO, Shop>(shop),
+            FactoryDTO factory => mapper.MapDTO<FactoryDTO, Factory>(factory),
+            HotelDTO hotel => mapper.MapDTO<HotelDTO, Hotel>(hotel),
+            WarehouseDTO warehouse => mapper.MapDTO<WarehouseDTO, Warehouse>(warehouse),
+            _ => null
+        };
+
+        return estate;
+    }
+}
---
+++ RealEstateBLL/EstateMapper.cs
@@ -0,0 +1,99 @@
+﻿// Created by Pontus Åkerberb 2024-10-07
+using AutoMapper;
+using RealEstateDTO;
+using RealEstateBLL.Estates;
+using RealEstateBLL.Persons;
+using RealEstateBLL.Payments;
+
+namespace RealEstateBLL;
+
+public class EstateMapper
+{
+    private IMapper _mapper;
+
+    public EstateMapper()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Address, AddressDTO>();
+            cfg.CreateMap<Person, PersonDTO>().IncludeAllDerived();
+            cfg.CreateMap<Seller, SellerDTO>();
+            cfg.CreateMap<Buyer, BuyerDTO>()
+                .ForMember(dest => dest.Payment, opt => opt.MapFrom(src => src.Payment));
+            cfg.CreateMap<Payment, PaymentDTO>()
+                .IncludeAllDerived()
+                .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount));
+            cfg.CreateMap<Bank, BankDTO>();
+            cfg.CreateMap<Paypal, PaypalDTO>();
+            cfg.CreateMap<WesternUnion, WesternUnionDTO>();
+            cfg.CreateMap<Estate, EstateDTO>()
+                .IncludeAllDerived()
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+                .ForMember(dest => dest.Seller, opt => opt.MapFrom(src => src.Seller))
+                .ForMember(dest => dest.Buyer, opt => opt.MapFrom(src => src.Buyer));
+            cfg.CreateMap<Residential, ResidentialDTO>()
+                .IncludeAllDerived();
+            cfg.CreateMap<Commercial, CommercialDTO>()
+                .IncludeAllDerived();
+            cfg.CreateMap<Institutional, InstitutionalDTO>()
+                .IncludeAllDerived();
+            cfg.CreateMap<Villa, VillaDTO>()
+                .IncludeAllDerived();
+            cfg.CreateMap<Rowhouse, RowhouseDTO>();
+            cfg.CreateMap<Apartment, ApartmentDTO>()
+                .IncludeAllDerived();
+            cfg.CreateMap<Rental, RentalDTO>();
+            cfg.CreateMap<Tenement, TenementDTO>();
+            cfg.CreateMap<Hospital, HospitalDTO>();
+            cfg.CreateMap<School, SchoolDTO>();
+            cfg.CreateMap<University, UniversityDTO>();
+            cfg.CreateMap<Shop, ShopDTO>();
+            cfg.CreateMap<Factory, FactoryDTO>();
+            cfg.CreateMap<Warehouse, WarehouseDTO>();
+            cfg.CreateMap<AddressDTO, Address>();
+            cfg.CreateMap<PersonDTO, Person>().IncludeAllDerived();
+            cfg.CreateMap<SellerDTO, Seller>();
+            cfg.CreateMap<BuyerDTO, Buyer>()
+                .ForMember(dest => dest.Payment, opt => opt.MapFrom(src => src.Payment));
+            cfg.CreateMap<PaymentDTO, Payment>()
+                .IncludeAllDerived()
+                .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount));
+            cfg.CreateMap<BankDTO, Bank>();
+            cfg.CreateMap<PaypalDTO, Paypal>();
+            cfg.CreateMap<WesternUnionDTO, WesternUnion>();
+            cfg.CreateMap<EstateDTO, Estate>()
+                .IncludeAllDerived()
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+                .ForMember(dest => dest.Seller, opt => opt.MapFrom(src => src.Seller))
+                .ForMember(dest => dest.Buyer, opt => opt.MapFrom(src => src.Buyer));
+            cfg.CreateMap<ResidentialDTO, Residential>()
+                .IncludeAllDerived();
+            cfg.CreateMap<CommercialDTO, Commercial>()
+                .IncludeAllDerived();
+            cfg.CreateMap<InstitutionalDTO, Institutional>()
+                .IncludeAllDerived();
+            cfg.CreateMap<VillaDTO, Villa>()
+                .IncludeAllDerived();
+            cfg.CreateMap<RowhouseDTO, Rowhouse>();
+            cfg.CreateMap<ApartmentDTO, Apartment>()
+                .IncludeAllDerived();
+            cfg.CreateMap<RentalDTO, Rental>();
+            cfg.CreateMap<TenementDTO, Tenement>();
+            cfg.CreateMap<HospitalDTO, Hospital>();
+            cfg.CreateMap<SchoolDTO, School>();
+            cfg.CreateMap<UniversityDTO, University>();
+            cfg.CreateMap<ShopDTO, Shop>();
+            cfg.CreateMap<FactoryDTO, Factory>();
+            cfg.CreateMap<WarehouseDTO, Warehouse>();
+        });
+
+        config.AssertConfigurationIsValid();
+
+        _mapper = config.CreateMapper();
+    }
+
+    public TDestination MapDTO<TSource, TDestination>(TSource source)
+    {
+        return _mapper.Map<TDestination>(source);
+    }
+}
